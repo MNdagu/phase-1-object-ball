b43d8b5dbb8d7f1be2725987e65fc93fbd17a214
MNdagu <ndagumark@gmail.com>
2024-07-20 20:08:42 +0300

    final

---
+++ src/00-objectball.js
@@ -0,0 +1,226 @@
+function gameObject() {
+  return {
+    home: {
+      teamName: 'Brooklyn Nets',
+      colors: ['Black', 'White'],
+      players: {
+        'Alan Anderson': {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1
+        },
+        'Reggie Evans': {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slamDunks: 7
+        },
+        'Brook Lopez': {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 15
+        },
+        'Mason Plumlee': {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slamDunks: 5
+        },
+        'Jason Terry': {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slamDunks: 1
+        }
+      }
+    },
+    away: {
+      teamName: 'Charlotte Hornets',
+      colors: ['Turquoise', 'Purple'],
+      players: {
+        'Jeff Adrien': {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slamDunks: 2
+        },
+        'Bismak Biyombo': {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slamDunks: 10
+        },
+        'DeSagna Diop': {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slamDunks: 5
+        },
+        'Ben Gordon': {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slamDunks: 0
+        },
+        'Brendan Haywood': {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slamDunks: 12
+        }
+      }
+    }
+  };
+}
+
+function homeTeamName() {
+  let object = gameObject();
+  return object["home"]["teamName"];
+}
+
+
+function numPointsScored(playerName) {
+  const game = gameObject();
+
+  if (game.home.players[playerName]) {
+    return game.home.players[playerName].points;
+  }
+
+  if (game.away.players[playerName]) {
+    return game.away.players[playerName].points;
+  }
+
+}
+
+//console.log(numPointsScored('Alan Anderson')); 
+
+function shoeSize(playerName) {
+  const game = gameObject();
+  if (game.home.players[playerName]) {
+    return game.home.players[playerName].shoe;
+  }
+
+  if (game.away.players[playerName]) {
+    return game.away.players[playerName].shoe;
+  }
+
+
+}
+//console.log(shoeSize('Alan Anderson')); 
+
+function teamColors(teamname) {
+  const game = gameObject();
+  if (game.home.teamName == teamname) {
+    return (game.home.colors)
+  }
+  if (game.away.teamName == teamname) {
+    return (game.away.colors)
+  }
+}
+teamColors('Charlotte Hornets');
+
+function teamNames() {
+  let game = gameObject();
+
+  return [game.home.teamName, game.away.teamName];
+}
+teamNames();
+
+function playerNumbers(teamName) {
+  const game = gameObject();
+  let team = null;
+
+  if (game.home.teamName === teamName) {
+    team = game.home;
+  } else if (game.away.teamName === teamName) {
+    team = game.away;
+  }
+
+  if (team) {
+    return Object.values(team.players).map(player => player.number);
+  }
+
+  return [];
+}
+
+playerNumbers('Brooklyn Nets');  
+
+function playerStats(playerName){
+  let game = gameObject()
+  if(game.home.players[playerName]){
+    return game.home.players[playerName]
+  }
+  if(game.away.players[playerName]){
+    return game.away.players[playerName]
+  }
+
+
+}
+playerStats("Alan Anderson");
+
+
+function bigShoeRebounds() {
+  const game = gameObject();
+  let largestShoeSize = 0;
+  let rebounds = 0;
+
+  function checkPlayers(players) {
+    for (let player in players) {
+      if (players.hasOwnProperty(player)) {
+        if (players[player].shoe > largestShoeSize) {
+          largestShoeSize = players[player].shoe;
+          rebounds = players[player].rebounds;
+        }
+      }
+    }
+  }
+
+  checkPlayers(game.home.players);
+  checkPlayers(game.away.players);
+
+  return rebounds;
+}
+
+console.log(bigShoeRebounds()); 
